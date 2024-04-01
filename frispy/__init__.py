29951435862a315f1743f30d209dfbf043903627
Hyperoot <hyperoot.tech@proton.me>
2024-04-01 11:06:42 +0530

    imported Disc, EOM, and Model to init file

--- frispy/__init__.py
+++ frispy/__init__.py
@@ -1,5 +1,12 @@
 """Frispy module."""
 
+from disc import Disc
+from equations_of_motion import EOM
+from model import Model
+
 # TODO: figure out how to use poetry-bumpversion
 # to keep this in line with the pyproject.toml
 __version__ = "2.0.0"
+
+
+__all__ = ["Disc", "EOM", "Model"]
